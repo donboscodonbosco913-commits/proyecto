a33fcdec4ae90e874cf79a42646e9944c4d3ea13
donboscodonbosco913-commits <donboscodonbosco913@gmail.com>
2025-10-02 16:49:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -543,6 +543,7 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Render define el puerto en la variable de entorno PORT
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
